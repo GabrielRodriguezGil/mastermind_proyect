b6cc27bc90c72c2099069936ed85a46b00b698b4
MiguelPhino <mpahinogutierrez@gmail.com>
2025-12-10 14:07:22 +0100

    chore(input_user): :art: Cambiar el valor de la variable de los colores

--- src/input_user.py
+++ src/input_user.py
@@ -1,13 +1,13 @@
 def color_input():
     user_input = (
         input(
-            "Introduce 4 colores separados por espacios de estos(rojo, azul, verde, amarillo, morado, naranja): "
+            "Introduce 4 colores separados por espacios de estos(rojo, azul, verde, amarillo, morado, blanco): "
         )
         .lower()
         .split()
     )
 
-    colors = ["rojo", "azul", "verde", "amarillo", "morado", "naranja"]
+    colors = ["rojo", "azul", "verde", "amarillo", "morado", "blanco"]
     colors_set = set(colors)
 
     user_input_set = set(user_input)
