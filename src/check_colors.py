704279c9ae1b661157f6686301a0ea6d99cda360
MiguelPhino <mpahinogutierrez@gmail.com>
2025-12-10 10:30:55 +0100

    fix(check_colors): :bug: Arreglar un pequeño error en el enumerate

--- src/check_colors.py
+++ src/check_colors.py
@@ -1,7 +1,7 @@
 
 def check_colors(color_combination , solution):
     successe_percentage = 0
-    for color, position in enumerate(color_combination):
+    for position, color in enumerate(color_combination):
         if color == solution[position]:
             successe_percentage += 1
         elif color in solution:
