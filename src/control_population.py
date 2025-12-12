857e3f5346107ce6dd4762271dd8f1e198ca13e7
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2025-12-12 13:01:36 +0100

    fix(control_population): :bug: Fixear un bug, en el codigo que no contemplaba a los hijos.

--- src/control_population.py
+++ src/control_population.py
@@ -5,18 +5,13 @@ def control_population(generation, fitness_values, childs):
     last_generation = generation.copy()
     fitness_values_copy = fitness_values.copy()
 
-    null_deaths = 0
-    for index, fitness in enumerate(fitness_values_copy):
-        if fitness == 0:
-            del last_generation[index]
-            del fitness_values_copy[index]
-        null_deaths += 1
-
-    fitness_inversed = [1 / v for v in fitness_values_copy]
+    fitness_inversed = [1 / fitness_value for fitness_value in fitness_values_copy]
 
     deaths = random.choices(
-        last_generation, weights=fitness_inversed, k=int((len(childs) - null_deaths))
+        last_generation, weights=fitness_inversed, k=int(len(childs))
     )
 
-    new_generation = [x for x in last_generation if x not in deaths]
+    new_generation = [
+        individual for individual in last_generation if individual not in deaths
+    ] + childs
     return new_generation
