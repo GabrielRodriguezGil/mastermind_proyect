c7cf54c7c620e920b9b462adbc21cca76a07b93c
MiguelPhino <mpahinogutierrez@gmail.com>
2025-12-12 13:14:32 +0100

    fix(check_colors): :bug: Arreglar  la escala de valores

--- src/check_colors.py
+++ src/check_colors.py
@@ -3,8 +3,10 @@ def check_colors(color_combination , solution):
     successe_percentage = 0
     for position, color in enumerate(color_combination):
         if color == solution[position]:
-            successe_percentage += 2
+            successe_percentage += 3
         elif color in solution:
+            successe_percentage += 2
+        else:
             successe_percentage += 1
             
     return successe_percentage
