1872cc056f00e09e76155258b39dab49292c3b4d
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2025-12-18 13:29:14 +0100

    fix(fitness_dictionary): :bug: Fixear bug de clave en el diccionario

--- src/create_fitness_dictionary.py
+++ src/create_fitness_dictionary.py
@@ -4,5 +4,7 @@ from check_colors import check_colors
 def create_fitness_dictionary(population, solution):
     fitness_dictionary = {}
     for individual in population:
-        fitness_dictionary[population] = check_colors(individual, solution)
+        individual = "".join(individual)
+        if individual not in fitness_dictionary:
+            fitness_dictionary[individual] = check_colors(individual, solution)
     return fitness_dictionary
