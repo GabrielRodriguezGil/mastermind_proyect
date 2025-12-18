3ef2aa9ec2c834739ac201498aea202213251927
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2025-12-18 15:07:38 +0100

    fix(control_population): :bug: Arreglar problemas que conflicten con otros modulos

--- src/control_population.py
+++ src/control_population.py
@@ -1,17 +1,16 @@
 import random
 
 
-def control_population(generation, fitness_values, childs, fitness_childs):
-    
-    new_generation = generation.copy() + childs
-    fitness_values_copy = fitness_values.copy() + fitness_childs
+def control_population(generation, fitness_values, childs):
+    new_generation = generation.copy()
+    fitness_values_copy = fitness_values.copy()
 
     fitness_inverted = [1 / fitness_value for fitness_value in fitness_values_copy]
 
-    for _ in range(len(childs)):   
+    for _ in range(len(childs)):
         elegido = random.choices(new_generation, weights=fitness_inverted, k=1)[0]
         index = new_generation.index(elegido)
         new_generation.pop(index)
         fitness_inverted.pop(index)
-        
+
     return new_generation
