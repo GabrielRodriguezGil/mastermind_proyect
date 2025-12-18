81c90439918bd7ca6592005c570186b6ac85d95a
MiguelPahino <mpahinogutierrez@gmail.com>
2025-12-18 17:32:41 +0100

    refactor(mix_generations): :recycle: Refactorizar el código para que la reproducción de los padres sea más efectiva

--- src/mix_generations.py
+++ src/mix_generations.py
@@ -8,8 +8,8 @@ def mix_generations(parents):
     parents_copy = parents[:]
     while 0 < len(parents_copy):
         pair_parents = random.sample(parents_copy, 2)
-        childs.append(pair_parents[0][0:2] + pair_parents[1][2:4])
-        childs.append(pair_parents[1][0:2] + pair_parents[0][2:4])
+        childs.append(pair_parents[0][0] + pair_parents[1][1]+ pair_parents[0][2] + pair_parents[1][3])
+        childs.append(pair_parents[1][0] + pair_parents[0][1] + pair_parents[1][2] + pair_parents[0][3])
         parents_copy.remove(pair_parents[0])
         parents_copy.remove(pair_parents[1])
 
