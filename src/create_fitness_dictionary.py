2a4083b0802a5b8b5071c27fece686dfea102830
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2025-12-18 15:03:06 +0100

    fix(fitness_dictionary): :bug: Arreglar bugs que impedian el funcionamiento del main.

--- src/create_fitness_dictionary.py
+++ src/create_fitness_dictionary.py
@@ -4,7 +4,9 @@ from check_colors import check_colors
 def create_fitness_dictionary(population, solution):
     fitness_dictionary = {}
     for individual in population:
-        individual = "".join(individual)
+        individual = " ".join(individual)
         if individual not in fitness_dictionary:
-            fitness_dictionary[individual] = check_colors(individual, solution)
+            fitness_dictionary[individual] = check_colors(
+                individual.split(" "), solution
+            )
     return fitness_dictionary
--- src/interpretate_dictionary.py
+++ src/interpretate_dictionary.py
@@ -1,6 +1,6 @@
 def interpretate_dictionary(fitness_dictionary, population):
     fitness_list = []
     for individual in population:
-        individual = "".join(individual)
+        individual = " ".join(individual)
         fitness_list.append(fitness_dictionary[individual])
     return fitness_list
