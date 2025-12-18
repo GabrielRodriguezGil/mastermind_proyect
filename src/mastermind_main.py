844672c8c2b2075fddb83f0b8db32ea8f8396f34
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2025-12-18 10:09:04 +0100

    feat(mastermind_main): :sparkles:

---
+++ src/mastermind_main.py
@@ -0,0 +1,34 @@
+from input_user import color_input
+from generate_combinations import generate_conbinations
+from create_fitness_dictionary import create_fitness_dictionary
+from select_parents import select_parents
+from mix_generations import mix_generations
+from control_population import control_population
+from interpretate_dictionary import interpretate_dictionary
+from return_colors import return_colors
+
+
+def mastermind():
+    solution = color_input()
+    BEST_FITNESS = 12
+    best_try = 0
+    tries = 0
+    last_population = generate_conbinations()
+    while best_try != BEST_FITNESS and tries < 14:
+        fitness_dictionary = create_fitness_dictionary(last_population, solution)
+        fitness_values = interpretate_dictionary(fitness_dictionary, last_population)
+        parents = select_parents(last_population, solution)
+        childs = mix_generations(parents)
+        fitness_dictionary = create_fitness_dictionary(
+            last_population + childs, solution
+        )
+        fitness_values = interpretate_dictionary(fitness_dictionary, last_population)
+        next_population = control_population(last_population, fitness_values, childs)
+        tries += 1
+        last_population = next_population
+        best_try = max(fitness_values)
+        print(return_colors(last_population[fitness_values.index(best_try)]))
+
+
+if __name__ == "__main__":
+    mastermind()
