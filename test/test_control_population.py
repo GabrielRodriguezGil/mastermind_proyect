4978dd21d4b732bf45eac4e4c1d99c21eb523030
goskus23 <141594539+goskus23@users.noreply.github.com>
2025-12-18 17:58:54 +0100

    test(test): :white_check_mark: Cambiar los casos test segun las nuevas implementaciones

--- test/test_control_population.py
+++ test/test_control_population.py
@@ -12,9 +12,17 @@ population = [
     ["verde", "morado", "rojo", "rojo"],
     ["verde", "verde", "amarillo", "rojo"],
     ["amarillo", "rojo", "azul", "amarillo"],
+    ["rojo", "verde", "azul", "amarillo"],
+    ["amarillo", "azul", "verde", "azul"],
+    ["rojo", "morado", "blanco", "rojo"]
+]
+childs = [
+    ["rojo", "verde", "azul", "amarillo"],
+    ["amarillo", "azul", "verde", "azul"],
+    ["rojo", "morado", "blanco", "rojo"],
 ]
 
 
 @pytest.mark.test_control_population
 def test_control_population():
-    assert len(control_population(population)) == len(population)
+    assert len(control_population(population,[1, 4, 3, 3, 4, 3, 4, 3, 5, 6,4,5,6],childs)) == len(population)-len(childs)
