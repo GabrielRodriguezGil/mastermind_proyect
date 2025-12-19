3cdf908abcbaa9571fc4f551bb0f0df9b3bba23d
MiguelPhino <mpahinogutierrez@gmail.com>
2025-12-19 12:20:18 +0100

    refactor(generate_combinations): :recycle: Refacrizar la cantidad de individuos que crea en la primera generación

--- src/generate_combinations.py
+++ src/generate_combinations.py
@@ -3,5 +3,5 @@ import random
 def generate_conbinations():
     colors = ["rojo", "azul", "verde", "amarillo", "morado", "blanco"]
     
-    return [[random.choice(colors)  for i in range(4)]for i in range(80)]
+    return [[random.choice(colors)  for i in range(4)]for i in range(100)]
 
